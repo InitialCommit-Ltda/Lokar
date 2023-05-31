a12bbe39818824d39c53aee93570d1de4e622df8
Ismael Rios <ismaelvrios1993@gmail.com>
2023-05-30 21:37:17 -0300

    Recomeço de projeto

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './assets/img/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Em construção
+        </h1>
+        <p>
+          Edite <code>src/App.js</code> e salve para recarregar.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
