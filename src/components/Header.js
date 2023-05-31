9e1aac3bf38a0d34103748d2829f5dabda98fc30
Ismael Rios <ismaelvrios1993@gmail.com>
2023-05-31 19:00:20 -0300

    Hheader e tentativa de inclusão de bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,32 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/img/logo.png';
+import './Header.css';
 
 function Header() {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contato">Contato</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/cadastro">Cadastro</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="logo-container">
+                    <Link to="/">
+                        <img src={logo} className="logo" alt="logo" />
+                    </Link>
+                </div>
+                <nav className="nav">
+                    <ul className="nav-list">
+                        <li className="nav-item">
+                            <Link to="/contato">Contato</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/cadastro">Cadastro</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
