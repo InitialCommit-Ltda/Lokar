9e57c85076663b8fb8426845c5b914b8249ea9d4
Ismael Rios <ismaelvrios1993@gmail.com>
2023-06-01 18:50:55 -0300

    Inclusão de bootstrap e alterações no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import logo from '../assets/img/logo.png';
 import '../styles/components.css';
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className="header">
-            <div className="container">
-                <div className="logo-container">
+            <div className="row">
+                <div className="logo-container col-2">
                     <Link to="/">
                         <img src={logo} className="logo" alt="logo" />
                     </Link>
                 </div>
-                <nav className="nav">
-                    <ul className="nav-list">
+                <nav className={`nav col-9 ${menuOpen ? 'open' : ''}`}>
+                    <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
                         <li className="nav-item">
                             <Link to="/">Home</Link>
                         </li>
@@ -26,6 +33,11 @@ function Header() {
                             <Link to="/cadastro">Cadastro</Link>
                         </li>
                     </ul>
+                    <div className="menu-toggle" onClick={toggleMenu}>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </div>
                 </nav>
             </div>
         </header>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,7 @@
-import logo from '../assets/img/logo.png';
-
 function Home() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <h1>
                     HOME
                 </h1>
