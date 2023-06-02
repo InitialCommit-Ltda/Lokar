a139921fe2f06499b65d05627079063f2f80b4d9
Faellz <rafael-h_@hotmail.com>
2023-06-02 15:30:02 -0300

    Um começo de tela de cadastro.

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -2,14 +2,23 @@ function Cadastro() {
     return (
         <div className="App">
             <header className="App-header">
+                
                 <h1>
                     CADASTRO
                 </h1>
                 <p>
-                    Edite: <code>src/pages/Cadastro.js</code>
+                    <input type="text" placeholder= "Username"></input>
                 </p>
+                <p>
+                    <input type="text" placeholder= "E-mail"></input>
+                </p>
+                <p>
+                    <input type="Text" placeholder= "Password"></input>
+                </p>
+                <button id= "meuBotao">Finalizar</button>
             </header>
         </div>
+
     );
 }
 
