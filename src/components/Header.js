8072213a89e01d110423ee15cb9c0959891012d5
Ismael Rios <ismaelvrios1993@gmail.com>
2023-06-07 19:02:02 -0300

    Alterações no estilo, header e home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,16 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import logo from '../assets/img/logo.png';
 import user from '../assets/icon/user.svg'
 import '../styles/components.css';
 
 function Header() {
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
     return (
         <header className="header">
-            <div className="blank-space" />
+            <div className="menu-toggle">
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
             <div className="logo">
                 <Link to="/">
                     <img src={logo} className="logo" alt="logo" />
@@ -27,41 +24,13 @@ function Header() {
                     <Link to="/contato">Contato</Link>
                 </li>
             </nav>
-            <div className="login-icon">
+            <div className="login">
                 <Link to="/login">
                     <img src={user} className="login-icon" alt="login" /> Login
                 </Link>
             </div>
-            <div className="blank-space" />
         </header>
     );
 }
-/*
-    <div className="row">
-                <div className="logo-container col-2 offset-1">
-                    <Link to="/">
-                        <img src={logo} className="logo" alt="logo" />
-                    </Link>
-                </div>
-                <nav className={`nav col-8 ${menuOpen ? 'open' : ''}`}>
-                    <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
-                        <li className="nav-item">
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/contato">Contato</Link>
-                        </li>
-                        <li className="nav-item">
 
-                            <Link to="/login"><img src={user} className="login-icon" alt="login" /> Login</Link>
-                        </li>
-                    </ul>
-                    <div className="menu-toggle" onClick={toggleMenu}>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                    </div>
-                </nav>
-            </div>
-*/
 export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,22 @@ import '../styles/pages.css'
 
 function Home() {
     return (
-        <div className="App">
-            <header className="Home-busca">
-                <form>
-                    <input type='date'>
-                    </input>
-                </form>
-            </header>
-            <div className="App-header">
+        <div className="Page">
+            <div className="container-busca">
+                <div className="busca">
+                    <form>
+                        <input type='date' placeholder='Data'>
+                        </input>
+                    </form>
+                </div>
+            </div>
+            
+            <div className="carousel">
                 <h1>
                     CAROUSEL
                 </h1>
             </div>
+            
         </div>
     );
 }
