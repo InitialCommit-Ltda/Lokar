47adecb3ab95a3a3ae3e64e8592fb966269aeaa0
Felipe de Sales <felipe_sales_@hotmail.com>
2023-06-09 21:41:19 -0300

    alterações na tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,25 +3,24 @@ import '../styles/pages.css';
 
 function Login() {
     return (
-        <div className="App">
-            <header className="Page">
+        <div className="login">
+            <header className="page">
                 <h1>Login</h1>
-                <form>
+                <form method='POST'class="formLogin">
                     <label for="email">E-mail</label>
-                    <input type="email"></input><br />
+                    <input type="email"></input>
                     <label for="senha">Senha</label>
-                    <input type="password"></input><br />
+                    <input type="password"></input>
                     <button>Acessar</button>
-                </form>
-
-                <p className='login-cadastro'>
-                    Não é cadastrado?
+                    
+                    <p className='login-cadastro'>
+                    Não é cadastrado? 
                     <Link to="/cadastro" className='login-cadastro-link'>
                         Cadastre-se!
                     </Link>
                 </p>
 
-
+                </form>
             </header>
         </div>
     );
