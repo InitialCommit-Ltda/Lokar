a19074713b5ad4813c5fcb9fafb6df645a4a17f8
Ismael Rios <ismaelvrios1993@gmail.com>
2023-06-20 19:00:28 -0300

    Ajuste de slides

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../styles/pages/home.css'
+import slide1 from '../assets/img/cars/onix-plus-premier-at-turbo.png'
+import slide2 from '../assets/img/cars/corolla xei.png'
 
 function Home() {
     const settings = {
@@ -27,14 +29,14 @@ function Home() {
 
             <div className="carousel">
                 <Slider {...settings}>
-                    <div>
-                        <h1>Slide 1</h1>
+                    <div className='slides'>
+                        <img src={slide1} alt="slide1" />
                     </div>
-                    <div>
-                        <h1>Slide 2</h1>
+                    <div className='slides'>
+                        <img src={slide2} alt="slide2" />
                     </div>
-                    <div>
-                        <h1>Slide 3</h1>
+                    <div className='slides'>
+                        <img src={slide2} alt="slide3" />
                     </div>
                 </Slider>
             </div>
