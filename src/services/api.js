b9b5154bc1be9711b11b759a290631862c868d4f
Ismael Rios <ismaelvrios1993@gmail.com>
2023-06-26 19:08:58 -0300

    Ajustes na API

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3002/', // URL base da sua API
-  // Outras configurações, como headers ou autenticação, podem ser adicionadas aqui
+  baseURL: 'http://lokar.ddns.net:8099/',
 });
 
 export const login = async (email, senha) => {
   try {
     const response = await api.post('/login', { email, senha });
-    console.log('DEU CERTO');
-    return response.data; // Retorna a resposta da API para ser tratada no componente
+    return response.data;
   } catch (error) {
-    throw new Error('Erro ao fazer login'); // Lança uma exceção em caso de erro
+    throw new Error('Erro na comunicação com a API: Não foi possível estabelecer conexão com o servidor');
   }
 };
-
-// Outras funções para interagir com a API podem ser adicionadas aqui
 
 export default api;
